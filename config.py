aa8bca74fefc4fc862d81d40deeda95d7de89eb9
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-08 20:15:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,8 @@ if not GROUP_CHAT_IDS:
 MAIN_GROUP_CHAT_ID = GROUP_CHAT_IDS[0]
 
 # Guruh nomlari (ixtiyoriy)
-GROUP_NAMES = {}
+GROUP_NAMES = {-1001716132943: "Toshkent Attestatsiya",
+    -1003903298311: "Yangi Guruh"}
 for idx, group_id in enumerate(GROUP_CHAT_IDS, 1):
     # Avval maxsus nomni qidirish
     custom_name = os.getenv(f"GROUP_NAME_{abs(group_id)}", "")
